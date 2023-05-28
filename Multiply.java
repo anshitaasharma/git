657d3decd7cbab9545b1537d1bfc712c4f035738
Anshita sharma <anshitasharma200318@gmail.com>
2023-05-28 17:23:59 +0530

    added message in multipl.java

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package git;
+
+public class Multiply {
+    public static void main (string[] args){
+        int a= 5;
+        int b = 6;
+        System.out.println("the answer for the sum is" a+b);
+   }
+       
+   
+    
+}
