f996c7b23e4d23ca006a6a35312df6f4bce0dc3f
Anshita sharma <anshitasharma200318@gmail.com>
2023-05-28 18:05:48 +0530

    modigied again

--- sum.java
+++ sum.java
@@ -4,7 +4,8 @@ public class sum {
     public static void main (string[] args){
      int a= 5;
      int b = 6;
-     System.out.println("the answer for the sum is" a+b);
+     int c = 5;
+     System.out.println("the answer for the sum is" a+b+c);
 }
     
 }
